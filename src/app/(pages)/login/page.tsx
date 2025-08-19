75b9f95860aef52fce1efda91e37bd79fedae86c
amruth <amruthkrishna2023@gmail.com>
2025-08-19 21:55:20 +0530

    HEXCART = Release V1

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useAuth } from "@/contexts/LoginContext";
+import { useAuth } from "@/contexts/AuthContext";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Head from "next/head";
